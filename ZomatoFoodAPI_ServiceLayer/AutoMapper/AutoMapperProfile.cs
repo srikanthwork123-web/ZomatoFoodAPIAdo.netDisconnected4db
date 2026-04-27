8d5de76bdeccc60ceef61a7e72faf05577e0ab4e
Dell <Dell@DESKTOP-13B42NJ>
2026-04-27 09:29:51 +0530

    Automapper code Added

---
+++ ZomatoFoodAPI_ServiceLayer/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZomatoFoodAPI_BusinessEntities.Dtos;
+using ZomatoFoodAPI_BusinessEntities.Models;
+
+namespace ZomatoFoodAPI_ServiceLayer.AutoMapper
+{
+    //create one class and inherit this class from profile class of automapper library and
+    //then we will create the constructor for this class and inside the constructor we will create
+    //the mapping configuration for the source and destination objects using the CreateMap method
+    //of the AutoMapper library.
+    public class AutoMapperProfile:Profile
+    {
+//create the constructor for this class and inside the constructor we will create
+//the mapping configuration for the source and destination objects using the CreateMap method of the AutoMapper library.
+        public AutoMapperProfile()
+        {
+            //=================syntax for creating the mapping configuration=================
+            // CreateMap<SourceClass, DestinationClass>();
+            // Example:
+            // CreateMap<UserEntity, UserDTO>();
+            //===========================================
+CreateMap<FileUploadDTO,FileUpload>();//this is used to map the data of fileUploadDTO class object to fileUpload class object
+CreateMap<FileUpload, FileUploadDTO>();//this is used to map the data of fileUpload class object to fileUploadDTO class object
+        }
+    }
+}
--- ZomatoFoodAPI_ServiceLayer/FilesUploadService.cs
+++ ZomatoFoodAPI_ServiceLayer/FilesUploadService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,51 +15,82 @@ namespace ZomatoFoodAPI_ServiceLayer
     public class FilesUploadService : IFilesUploadService
     {
         public readonly IFilesUploadRepository _filesUploadRepository;
-        public FilesUploadService(IFilesUploadRepository filesUploadRepository)
+        private readonly IMapper _mapper;
+        public FilesUploadService(IFilesUploadRepository filesUploadRepository, IMapper mapper)
         {
             _filesUploadRepository = filesUploadRepository;
+            this._mapper = mapper;
         }
         public async Task<List<FileUploadDTO>> GetFileUploadList()
         {
             var fileUploadList = await _filesUploadRepository.GetFileUploadList();
-            List<FileUploadDTO> lstFileUploadDTO = new List<FileUploadDTO>();
-            foreach (var fileUpload in fileUploadList)
-            {
-                FileUploadDTO fileUploadDTO = new FileUploadDTO();
-                fileUploadDTO.Id = fileUpload.Id;
-                fileUploadDTO.FileName = fileUpload.FileName;
-                fileUploadDTO.ModifiedFilename = fileUpload.ModifiedFilename;
-                fileUploadDTO.FilePath = fileUpload.FilePath;
-                fileUploadDTO.Createdby = fileUpload.Createdby;
-                fileUploadDTO.CreatedDatetTime = fileUpload.CreatedDatetTime;
-                lstFileUploadDTO.Add(fileUploadDTO);
-            }
-            return lstFileUploadDTO;
+
+  #region before Automapper we are mapping the entiy class object to dto class object process
+  //          List<FileUploadDTO> lstFileUploadDTO = new List<FileUploadDTO>();
+  //          foreach (var fileUpload in fileUploadList)
+  //          {
+  //              FileUploadDTO fileUploadDTO = new FileUploadDTO();
+             
+
+  //              #region before Automapper we are mapping the entiy class object to dto class object process
+  //              //fileUploadDTO.Id = fileUpload.Id;
+  //              //fileUploadDTO.FileName = fileUpload.FileName;
+  //              //fileUploadDTO.ModifiedFilename = fileUpload.ModifiedFilename;
+  //              //fileUploadDTO.FilePath = fileUpload.FilePath;
+  //              //fileUploadDTO.Createdby = fileUpload.Createdby;
+  //              //fileUploadDTO.CreatedDatetTime = fileUpload.CreatedDatetTime;
+  //              #endregion region
+
+  //              lstFileUploadDTO.Add(fileUploadDTO);
+  //          }
+  //          return lstFileUploadDTO;
+            #endregion
+
+
+     //The above enire code was replaced by below single of code.
+     //var result= _mapper.Map<List<FileUploadDTO>>(fileUploadList);(for debugging purpose assign into one variable and you can check)
+            return _mapper.Map<List<FileUploadDTO>>(fileUploadList);//entity to dto mapping
         }
         public async Task<FileUploadDTO> GetFileUploadDetailsById(int Id)
-        {
+        {//if you are using any class as a return type of method,we must return the value of that class object.this is rule
             var result = await _filesUploadRepository.GetFileUploadDetailsById(Id);
-            FileUploadDTO fileUploadDTO = new FileUploadDTO();
-            fileUploadDTO.ModifiedFilename = result.ModifiedFilename;
-            fileUploadDTO.Id = result.Id;
-            fileUploadDTO.FileName = result.FileName;
-            fileUploadDTO.ModifiedFilename = result.ModifiedFilename;
-            fileUploadDTO.FilePath = result.FilePath;
-            fileUploadDTO.Createdby = result.Createdby;
-            fileUploadDTO.CreatedDatetTime = result.CreatedDatetTime;
-            return fileUploadDTO;
+            // 1)Auto mapper is used to create a mapping between  to source model object to destination model object        
+            return _mapper.Map<FileUploadDTO>(result);//entity to dto mapping
+//in Service layer we are using Dto(Data transfer object) classes and return the data of Dto class object data.
+            #region before Automapper we are mapping the entiy class object to dto class object process
+            //FileUploadDTO fileUploadDTO = new FileUploadDTO();
+            //fileUploadDTO.ModifiedFilename = result.ModifiedFilename;
+            //fileUploadDTO.Id = result.Id;
+            //fileUploadDTO.FileName = result.FileName;
+            //fileUploadDTO.ModifiedFilename = result.ModifiedFilename;
+            //fileUploadDTO.FilePath = result.FilePath;
+            //fileUploadDTO.Createdby = result.Createdby;
+            //fileUploadDTO.CreatedDatetTime = result.CreatedDatetTime;
+            //return fileUploadDTO;
+            #endregion
+
         }
 
         public async Task<FileUploadResponse> AddFileUpload(FileUploadDTO fileUploadDTO)
         {
             FileUpload obj = new FileUpload();//destinationmodelclass object
-          //This Code was replaced by above Automapper concept.
-            obj.FileName = fileUploadDTO.FileName;
-            obj.FilePath = fileUploadDTO.FilePath;
-            obj.CreatedDatetTime = fileUploadDTO.CreatedDatetTime;
-            obj.Createdby = fileUploadDTO.Createdby;
-            obj.ModifiedFilename = fileUploadDTO.ModifiedFilename;
-            obj.Id = fileUploadDTO.Id;
+                                              //This Code was replaced by above Automapper concept.
+// 1)Auto mapper is used to create a mapping between  to source model object to destination model object
+            _mapper.Map(fileUploadDTO, obj);//sourceobject,destinationobject
+//Converting source modelobject to destination modelobject
+//Syntax:   _mapper.Map(SourceModelObject,DestinationModelObject)
+//once mapping is created, source model object can be converted to destination model object with less code and easy way.
+            #region before Automapper we are mapping the entiy class object to dto class object process
+            //This Code was replaced by above Automapper concept.
+            //obj.FileName = fileUploadDTO.FileName;
+            //obj.FilePath = fileUploadDTO.FilePath;
+            //obj.CreatedDatetTime = fileUploadDTO.CreatedDatetTime;
+            //obj.Createdby = fileUploadDTO.Createdby;
+            //obj.ModifiedFilename = fileUploadDTO.ModifiedFilename;
+            //obj.Id = fileUploadDTO.Id;
+            #endregion
+
+
             var result = await _filesUploadRepository.AddFileUpload(obj);
             return result;
 
@@ -66,3 +98,32 @@ namespace ZomatoFoodAPI_ServiceLayer
     }
 
     }
+/* 1.what is Automapper?
+ 1)Auto mapper is used to create a mapping between  to source model object to destination model object
+
+ 2)once mapping is created, source model object can be converted to destination model object with less code and easy way.
+ 
+ 3)Auto mapper can be instaled by using NUEGet Manage packager.
+ 
+ 4) This required two steps:
+
+        1) creating mapping
+         
+      syntax: mapper.createmap < sourcemodel object,destination modelobject >();
+            
+           Here <> Means We called as a Placeholder .
+
+         2) Converting source modelobject to destination modelobject
+
+          destination modelclass Reference variable= Mapper.Map<destination modelclassname>(source modelclasspbject)
+
+//===================================================
+
+//Converting source modelobject to destination modelobject
+//Syntax:   _mapper.Map(SourceModelObject,DestinationModelObject)
+//once mapping is created, source model object can be converted to destination model object with less code and easy way.
+================
+
+
+
+*/
