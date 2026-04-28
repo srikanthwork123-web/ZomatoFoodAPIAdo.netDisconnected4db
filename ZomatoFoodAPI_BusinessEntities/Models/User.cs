29e1308ab6995a83de31ebe5ddd26460163abe84
Dell <Dell@DESKTOP-13B42NJ>
2026-04-28 09:38:18 +0530

    3rd party api url calling

---
+++ ZomatoFoodAPI_BusinessEntities/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZomatoFoodAPI_BusinessEntities.Models
+{
+    public  class User
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
+/*response
+ * ===========
+  {
+        "id": 1,
+        "userName": "User 1",
+        "password": "Password1"
+    }
+*/
