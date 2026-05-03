dba681328d783e0ca04fc3659c3f0769b0a305d4
Dell <Dell@DESKTOP-13B42NJ>
2026-05-03 09:51:22 +0530

    userstory-123456-(depencyinjection Descriptio added)

--- ZomatoFoodAPI/Controllers/EmployeeController.cs
+++ ZomatoFoodAPI/Controllers/EmployeeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using ZomatoFoodAPI_BusinessEntities.Dtos;
 using ZomatoFoodAPI_BusinessEntities.Interfaces;
+using ZomatoFoodAPI_RepositoryLayer;
+using ZomatoFoodAPI_ServiceLayer;
 
 namespace ZomatoFoodAPI.Controllers
 {
@@ -14,20 +16,21 @@ namespace ZomatoFoodAPI.Controllers
         //Depency injection used to avoid tightly coupling between the classes.
 
         //Dependency injection TYPES we can implement in 3 ways at controller level
-        //1.constructor injection(Realtime used 99%)
-        //2.property injection
-        //3.method injection
+        //1.constructor injection(Realtime used constructor injection 99%)
+        //2.property injection(not used in real time application development)
+        //3.method injection(not used in real time application development)
         //**You can use any one the type .realtime mainly used constructor injection
 
 
 
 
-        #region before dependency injection we are creating the object of the service class in the controller class to access the members of that service class process
-        //employeeservice obj=new employeeservice();//this called tightly coupling process (Don't create object of depency class directly).
+        #region before dependency injection we are creating the object of the service class in the controller class to access the members of that service class
+        //employeeservice obj=new employeeservice();//this called tightly coupling process (Don't create object of depency class directly into controller classes).
         //this process not used in real time application development because it will create tight coupling between the classes and it will create the dependency between the classes and it will create the maintenance issue in the application.
         #endregion
 
-        #region 1.constructor injection process
+        #region 1.constructor injection process (Realtime used 99%)this process
+        //************ClassDependencies we  are providing through the constructor of a class.*************
         //IN the  constructor Injection, inject/pass the dependecies to the constructor 
         //Injection means add your depencies to constructior.
         //all depenceies we are passing/Injecting into to the constructor.
@@ -46,11 +49,20 @@ namespace ZomatoFoodAPI.Controllers
 
         #endregion
 
-        #region 2.PropertyInjection process
+        #region 2.PropertyInjection process(but currently not used in real time application development)
         //In the property injection, the dependency is provided through a public property.(this way also you use)
         //public   IFilesUploadService _filesUploadService { get; set; }
         //private  IUserService _userService { get; set; } 
         //private  IEmployeeService _employeeService { get; set; }
+        #endregion
+
+        #region 3.MethodInjection process(but currently not used in real time application development)
+
+        //public IActionResult Index([FromServices] IEmployeeService _employeeService)
+        //{
+        //    return Ok();
+        //}
+
         #endregion
 
         [HttpGet]
@@ -184,14 +196,85 @@ namespace ZomatoFoodAPI.Controllers
 ========================================================================
 A)
 1.Dependency injection is a design pattern.
-2.which is used to develop the loosly coupling between the classes.(Loosly coulping means dont create direct obect of the repository class in the controller class to access the members of that repository class.)
+2.which is used to develop the loosly coupling between the classes.
 3)To implement loosly coupling between the classes ,by using interfaces and interface implemented classes we can achive the dependency injection.
 =================================================================
-4)Depenceny injection is avoid the tight coupling between the classes. that mens we can not create direct object of the class in another class to access the members of that class. 
+4)Depenceny injection is avoid the tightly coupling between the classes.
+that mens we can not create direct object of the class in another class to access the members of that class. 
 5)In old application development we are creating the object of the class in another class to access the members of that class.
 6)but in dependency injection we are not creating the object of the class in another class to access the members of that class.
 7)these interfaces we are injecting to controller class by using constructor injection.
-8)After that in program.cs we have inbulit depency dency injection  container is there,that is called builder and in that we need to register 
-the interface and its implemented classes into the dependency injection container of the application by using AddScoped or AddSingleton or AddTransient method of the builder.Services object.
+8)After that in program.cs we have inbulit depencyinjection  container available,that is called builder and in builder that we need to register our dependencies.,
+ interfacename and interface  implemented classes Register into builder.Services object using  any one of AddScoped or AddSingleton or AddTransient methods. 
+================********************************************************************************************************************************==========================
+2)Can you explain Dependency injection TYPES At Controller level?
+=============================================
+we can implement in 3 ways at controller level
+   //1.constructor injection
+   //2.property injection
+   //3.method injection
+//**You can use any one the type .realtime mainly used constructor injection.
+====================================================================================
+3)What is the constructor injection process?
+A)In the constructor injection, inject/pass the dependecies to the constructor,injection means add your depencies to constructior.class level dependencies  adding in constructor.
+create private readonly field of the interface type and then we can use that private readonly field to access the members of the service class in the controller class.
+======================================================================================================================
+4)Can you explain depencyinjection at program.cs level? (or) can you explain depencyinjectionservice life time methods?(or) can you explain service life time methods in depency injection?
+can you explain breifly about AddSingleton,AddScoped and AddTransient methods in depency injection?(or) what are the differences between AddSingleton,AddScoped and AddTransient methods in depency injection?
+==========================================================================================================================================================================================
+A)
+    In program.cs we have three types of dependency injection life time methods are there,they are:
+    1)AddSingleton
+    2)AddScoped
+    3)AddTransient
+======================
+1)AddSingleton method:
+Services.Addsingleton() method:
+===========================
+By using singleton service it created  only one object and it is used for all httpRequests.
+that same object is used for all the times.
+if you call  same method multiple times also it generates only one object in runtime.
+this is called singleton design pattern.
+for entire project we are reading the connection string only one time and that connection string information is used for all the times in the repository layer to connect to the database and perform the required crud operations.
+in my project we are registering the connection factory class in the dependency injection container of the application using the AddSingleton method because we are reading the connection string only one time and that connection string information is used for all the times in the repository layer to connect to the database and perform the required crud operations.
+builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+##########################################################################
+2)AddScoped method:
+Services.Addscoped() method:
+==========================
+By using Addscoped service every httprequest new instance/object  is created.
+(whether it is get(or) post(or)put(or)delete opertions, each and every request new instance/object is created for  scoped service).
+in my project we are registering the repository interfaces and their implementation classes and service interfaces and their implementation classes in the dependency injection container of the application using the AddScoped method because we want to create new instance/object for every http request to access the members of the repository and service classes in the controller class to perform the required operations.
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+###########################################################################
+3)AddTransient method:
+Services.AddTransient() method:
+===============================
+If any 3rd party apis comunication in your Api we can go for this  services.
+AddTransient() method.
+
+By using Add Transient service each and every request new instance is created.
+if you have any subsequent request it will create another object also.
+Note:In one api if you call another api  i.e is subsequent request.
+
+
+ Note:
+•	Services.Addscoped and  services.addtransient almost same.
+The main difference is if you have any subsequent request(or)3rdparty api calling  in your service go for services.add transient().
+if any database crud opertions in your service go for services.addscoped().
+ 
+
+//if you want to call any 3rd party api urls calling scenario use AddTraniscent();
+in my project we are using like below way to register the user repository and user service in the dependency injection container of the application using the AddTransient method because we are calling 3rd party api urls in the user repository class to get the data from that 3rd party api urls and then we are sending that data to the controller class and then we are sending that data to the client in a single response.
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+
+
+
+
+
+
 
 */
--- ZomatoFoodAPI/Program.cs
+++ ZomatoFoodAPI/Program.cs
@@ -5,30 +5,82 @@ using ZomatoFoodAPI_ServiceLayer;
 
 //Program.cs is the entry point of the application Here we are configuring/adding/registering the services and  middlewares to the application.
 //In this file we are adding/registering the services and the repositories in the dependency injection container of the application and then we are building the application and running it.
-//this program.cs is divided into 2 sections.
-//section1:builder is the inbuilt depency injection conatiner.we need to register our all application depencies into our inbuilt depency injection container.
-//this conatiner will load your depencies and then it will inject those depencies to the controller class by using constructor injection and then we can use those depencies in the controller class to perform the required operations.
-#region inbuilt dependency injection containerSection
-var builder = WebApplication.CreateBuilder(args);
 
+//this program.cs is divided into 2 sections.
+//===========================================================
+//section1:builder is the inbuilt depency injection conatiner.we need to register our all application depencies into our inbuilt depency injection container.
+//================================================================================================================================================================
+//this conatiner will load your depencies and then it will inject those depencies to the controller class by using constructor injection and then we can use those depencies in the controller class to perform the required operations.
+#region inbuilt dependency injection containerSection.
+var builder = WebApplication.CreateBuilder(args);//builder is the inbuilt dependency injection container which is used to register the services and the repositories in the dependency injection container of the application and then we are building the application and running it.
+//if you run the program,first it will call program.cs and it will load all the depencies into the memory and then it will inject those depencies to the controller class by using constructor injection and then we can use those depencies in the controller class to perform the required operations
 // If you want to add any depencencies to your  container. by using builder.services....we can register our dependicies.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();//this is used to load the swagger
-//==============************************************************************************
-builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
 
+//To implement the depency Injection must and stood register the interfacename,interfaceimplemented class here.
+//If you are not registered it will throw "System.InvalidOpertionException:Unable to reslove service type" Error
+//These interfaces we are injecting into controller constructor,to  implement the loosely coupling between the classes
+//*************WE MUST REGISTER THE INTERFACENAME,INTERFACEIMPLEMNTEDCHILDNAME CLASS NAME HERE to implement depency injection**************
+//==============************************************************************************
+
+
+#region AddSingletonServiceMethod
+/*
+Services.Addsingleton() method:
+===========================
+By using singleton service it created  only one object and it is used for all httpRequests.
+that same object is used for all the times.
+if you call  same method multiple times also it generates only one object in runtime.
+this is called singleton design pattern.
+for entire project we are reading the connection string only one time and that connection string information is used for all the times in the repository layer to connect to the database and perform the required crud operations.
+*/
+//Syntax for registering:builder.Services.AddSingleton<InterfaceName,InterfaceImplementedClassName>();
+//========================================================================================================
+builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();//<>  we called this as placeholder(or)angle brackets.
+
+#endregion
+
+#region AddScopedService
+/*
+Services.Addscoped() method:
+==========================
+By using Addscoped service every httprequest new instance/object  is created.
+(whether it is get(or) post(or)put(or)delete opertions, each and every request new instance/object is created for  scoped service).
+*/
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();//register the repository interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();//register the service interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
 
 builder.Services.AddScoped<IFilesUploadService, FilesUploadService>();
 builder.Services.AddScoped<IFilesUploadRepository, FilesUploadRepository>();
+#endregion
 
+#region  AddTransientService
+/*
+Services.AddTransient() method:
+===============================
+If any 3rd party apis comunication in your Api we can go for this  services.
+AddTransient() method.
+
+By using Add Transient service each and every request new instance is created.
+if you have any subsequent request it will create another object also.
+Note:In one api if you call another api  i.e is subsequent request.
+
+
+ Note:
+•	Services.Addscoped and  services.addtransient almost same.
+The main difference is if you have any subsequent request(or)3rdparty api calling  in your service go for services.add transient().
+if any database crud opertions in your service go for services.addscoped().
+ 
+ */
 //if you want to call any 3rd party api urls calling scenario use AddTraniscent();
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
+#endregion
+
 
 //===================***********************************************************************
 //Here we are adding/Registering the automapper in the dependency injection container of the application using the AddAutoMapper method of the builder.Services object and we are passing the assemblies of the application to the AddAutoMapper method to scan the profiles of automapper in those assemblies and then we can use the automapper in our application to map the entity class objects to dto class objects and vice versa.
@@ -41,7 +93,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 //section2:app is the inbuilt request pipeline,heare we need to register our middlewares to application pipeline.
-
+//===================================================================================================================
 #region inbuilt request pipelineSection
 var app = builder.Build();
 
